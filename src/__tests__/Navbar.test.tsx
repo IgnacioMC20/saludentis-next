ed723a5dac8bc2ea50f80c65d2700691e3d2c88b
Pamech02 <17005131@galileo.edu>
2024-07-05 23:46:17 -0600

    test: add unit test for Navbar component

--- src/__tests__/Navbar.test.tsx
+++ src/__tests__/Navbar.test.tsx
@@ -1,12 +1,24 @@
-import { render, screen } from '@testing-library/react'
-
-import { Navbar } from '@/components'
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; 
+import { Navbar } from '@/components';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 describe('Navbar component', () => {
     it('renders Navbar component', () => {
-        render(<Navbar />)
-        const navbarElement = screen.getByText(/Navbar/i)
-        expect(navbarElement).toBeInTheDocument()
-    })
+        
+        const theme = createTheme();
+        
+        render(
+            <ThemeProvider theme={theme}>
+                <Navbar />
+            </ThemeProvider>
+        );
+        
+        const logo = screen.getByAltText('Logo');
+        expect(logo).toBeInTheDocument();
 
-})
+
+        const logoText = screen.getByText(/SALUDENTIS/i);
+        expect(logoText).toBeInTheDocument();
+    });
+});
