fa4d27299f64f3391ab20156eb0320610f610fca
Ignacio Martínez <82549600+IgnacioMC20@users.noreply.github.com>
2024-08-07 23:51:54 -0600

    Add odontogram (#9)
    * feat: add odontogram to project
    
    * feat: add odontogram to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import 'react-toastify/dist/ReactToastify.css'
+import '../components/Odontogram/App.css'
 
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
